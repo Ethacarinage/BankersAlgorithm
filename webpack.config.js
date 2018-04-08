1fbd38d2eb42c965e46014f8c7127daa57679125
J. David Mena <davis82100@utp.edu.co>
2018-04-08 15:58:44 -0500

    Start Application

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	entry: './src/index.jsx',
+	output: {
+		path: __dirname + '/public/js',
+		filename: 'bundle.js'
+
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}
+		]
+	}
+
+}
